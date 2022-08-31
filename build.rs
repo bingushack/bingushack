1c810bf4a4b10937906a204a484924e134875328
majorsopa <73245063+majorsopa@users.noreply.github.com>
2022-08-31 10:06:01 -0400

    fixing some git shit

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
-const HWID: &str = "";
+// sopa's
+// const HWID: &str = "dee2ce704deb97081914ecca6d1555ba6bb5658a493afe65427f64b00fa2bf04dbc53e5baf1cc38a4e94203f45bc82a0e6acbd1abf11afa6a66b7ef7e13acc3b";
+const HWID: &str = "dee2ce704deb97081914ecca6d1555ba6bb5658a493afe65427f64b00fa2bf04dbc53e5baf1cc38a4e94203f45bc82a0e6acbd1abf11afa6a66b7ef7e13acc3b";
 
 use std::env;
 
