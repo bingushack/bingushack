f6fc0e3f60d1b4667615dae4c36b5e577b977eca
majorsopa <73245063+majorsopa@users.noreply.github.com>
2022-09-03 20:33:20 -0400

    one last oops

--- src/client/module/modules/mod.rs
+++ src/client/module/modules/mod.rs
@@ -6,4 +6,3 @@ pub use self::{autototem::*, testmodule::*};
 use super::{
     AllSettingsType, BingusModule, BingusSettings, BoxedBingusModule, SettingType, SettingValue,
 };
-use crate::client::mapping::MemTrait;
