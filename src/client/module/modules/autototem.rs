92321a6f5d0d873eb0476c5a61d7e6e6bd5ef2de
majorsopa <73245063+majorsopa@users.noreply.github.com>
2022-10-10 14:47:38 -0400

    fix 10th of a second maybe?

--- src/client/module/modules/autototem.rs
+++ src/client/module/modules/autototem.rs
@@ -37,8 +37,8 @@ impl BingusModule for AutoTotem {
             settings: Arc::new(Mutex::new(RefCell::new(vec![
                 Rc::new(RefCell::new(BingusSettings::RangeSetting(
                     RangeSetting::new(
-                        SettingValue::from([50.0, 240.0]),
-                        0.0..=200.0,
+                        SettingValue::from([500.0, 1000.0]),
+                        0.0..=2000.0,
                         Some(0),
                         Some(1.0),
                         "delay (10th of a second)",
@@ -60,7 +60,7 @@ impl BingusModule for AutoTotem {
                 let range_setting: RangeSetting = bingus_setting_ref.clone().try_into().unwrap();
                 range_setting.get_random_i64_in_range() as u128
             };
-            if time_since_lost_totem >= next_delay {
+            if time_since_lost_totem >= next_delay * 100 {
                 self.needing_swap_time = None;
             }
         }
