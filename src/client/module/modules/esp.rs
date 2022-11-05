0edd765456f50032e5ad257a9c203c1cdfe4fc62
majorsopa <73245063+majorsopa@users.noreply.github.com>
2022-11-05 19:05:02 -0400

    maybe

--- src/client/module/modules/esp.rs
+++ src/client/module/modules/esp.rs
@@ -4,14 +4,14 @@ use super::{
 use crate::{client::{
     mapping::MappingsManager,
     setting::{BooleanSetting, FloatSetting},
-}};
+}, NEW_CONTEXT, STATIC_HDC};
 use gl::{types::GLfloat};
 use jni::JNIEnv;
-use winapi::{shared::windef::{RECT, HDC}, um::winuser::{GetClientRect, WindowFromDC}};
+use winapi::{shared::windef::{RECT, HDC}, um::{winuser::{GetClientRect, WindowFromDC}, wingdi::{wglGetCurrentContext, wglMakeCurrent}}};
 use std::{
     cell::RefCell,
     rc::Rc,
-    sync::{Arc, Mutex}, ptr::null_mut,
+    sync::{Arc, Mutex}, ptr::null_mut, ffi::CString,
 };
 
 pub struct Esp {
@@ -38,20 +38,15 @@ impl BingusModule for Esp {
     fn tick(&mut self, _env: Rc<JNIEnv>, _mappings_manager: Rc<MappingsManager>) {}
 
     fn render_event(&self) {
-        /*
-        log_to_file("called render_event");
         // if enabled
-        if self.get_enabled()
-        {
-            log_to_file("render event start");
+        if self.get_enabled() {
             unsafe {
-                esp(*STATIC_HDC.lock().unwrap(), 1.0)
+                let local_new_context = NEW_CONTEXT.get_mut().unwrap();
+                let hdc = *STATIC_HDC.lock().unwrap();
+                wglMakeCurrent(hdc, *local_new_context.get_mut());
+                esp(hdc, 1.0)
             }
-            log_to_file("render event end");
-        } else {
-            log_to_file("render event not enabled");
         }
-        */
     }
 
     fn on_load(&mut self, _env: Rc<JNIEnv>, _mappings_manager: Rc<MappingsManager>) {}
@@ -75,15 +70,17 @@ impl BingusModule for Esp {
     }
 }
 
-fn esp(hdc: HDC, _alpha: GLfloat) {
-    let rc_cli: *mut RECT = null_mut();
+fn esp(hdc: HDC, _alpha: gl::types::GLfloat) {
+    /*
+    let rc_cli = null_mut();
     unsafe {
         GetClientRect(WindowFromDC(hdc), rc_cli);
     }
-    let rc_cli = unsafe { *rc_cli };
+    let rc_cli = unsafe { *rc_cli };  // crash
     let width = rc_cli.right - rc_cli.left;
     let height = rc_cli.bottom - rc_cli.top;
-    draw_triangle(width, height);
+    */
+    draw_triangle(hdc, 400, 400);
     /*
     unsafe {
         Viewport(0, 0, width, height);
@@ -91,8 +88,149 @@ fn esp(hdc: HDC, _alpha: GLfloat) {
     */
 }
 
-fn draw_triangle(_w: i32, _h: i32) {
+fn draw_triangle(hdc: HDC, _w: i32, _h: i32) {
     unsafe {
+        wglMakeCurrent(hdc, wglGetCurrentContext());
+    }
+
+    use gl::types::{GLfloat, GLenum, GLuint, GLchar, GLint, GLboolean, GLsizeiptr};
+    use std::str;
+    use std::ptr;
+    use std::mem;
+
+    static VERTEX_DATA: [GLfloat; 6] = [0.0, 0.5, 0.5, -0.5, -0.5, -0.5];
+    static VS_SRC: &str = "
+#version 150
+in vec2 position;
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+}";
+
+    static FS_SRC: &str = "
+#version 150
+out vec4 out_color;
+void main() {
+    out_color = vec4(1.0, 1.0, 1.0, 1.0);
+}";
+
+
+    fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+        let shader;
+        unsafe {
+            shader = gl::CreateShader(ty);
+            // Attempt to compile the shader
+            let c_str = CString::new(src.as_bytes()).unwrap();
+            gl::ShaderSource(shader, 1, &c_str.as_ptr(), ptr::null());
+            gl::CompileShader(shader);
+
+            // Get the compile status
+            let mut status = gl::FALSE as GLint;
+            gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+            // Fail on error
+            if status != (gl::TRUE as GLint) {
+                let mut len = 0;
+                gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+                let mut buf = Vec::with_capacity(len as usize);
+                buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+                gl::GetShaderInfoLog(
+                    shader,
+                    len,
+                    ptr::null_mut(),
+                    buf.as_mut_ptr() as *mut GLchar,
+                );
+                panic!(
+                    "{}",
+                    str::from_utf8(&buf)
+                        .ok()
+                        .expect("ShaderInfoLog not valid utf8")
+                );
+            }
+        }
+        shader
+    }
+
+    fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
+        unsafe {
+            let program = gl::CreateProgram();
+            gl::AttachShader(program, vs);
+            gl::AttachShader(program, fs);
+            gl::LinkProgram(program);
+            // Get the link status
+            let mut status = gl::FALSE as GLint;
+            gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+    
+            // Fail on error
+            if status != (gl::TRUE as GLint) {
+                let mut len: GLint = 0;
+                gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+                let mut buf = Vec::with_capacity(len as usize);
+                buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+                gl::GetProgramInfoLog(
+                    program,
+                    len,
+                    ptr::null_mut(),
+                    buf.as_mut_ptr() as *mut GLchar,
+                );
+                panic!(
+                    "{}",
+                    str::from_utf8(&buf)
+                        .ok()
+                        .expect("ProgramInfoLog not valid utf8")
+                );
+            }
+            program
+        }
+    }
+
+    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+    let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+    let program = link_program(vs, fs);
+
+    let mut vao = 0;
+    let mut vbo = 0;
+
+
+    unsafe {
+        // Create Vertex Array Object
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);  // crash
+
+
         
+        // Create a Vertex Buffer Object and copy the vertex data to it
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            mem::transmute(&VERTEX_DATA[0]),
+            gl::STATIC_DRAW,
+        );
+
+
+        // Use shader program
+        gl::UseProgram(program);
+        gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
+        
+        // Specify the layout of the vertex data
+        let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
+        gl::EnableVertexAttribArray(pos_attr as GLuint);
+        gl::VertexAttribPointer(
+            pos_attr as GLuint,
+            2,
+            gl::FLOAT,
+            gl::FALSE as GLboolean,
+            0,
+            ptr::null(),
+        );
+    }
+
+    unsafe {
+        // Clear the screen to black
+        gl::ClearColor(0.7, 0.1, 0.1, 1.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+        // Draw a triangle from the 3 vertices
+        gl::DrawArrays(gl::TRIANGLES, 0, 3);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,7 @@ static mut CLICKGUI_SENDER: Mutex<Option<Sender<ClickGuiMessage>>> = Mutex::new(
 static mut TX: Mutex<Option<Sender<Message>>> = Mutex::new(None);
 
 static WAITING_CELL: OnceCell<SystemTime> = OnceCell::new();
-static mut NEW_CONTEXT: OnceCell<AtomicPtr<HGLRC__>> = OnceCell::new();
+pub static mut NEW_CONTEXT: OnceCell<AtomicPtr<HGLRC__>> = OnceCell::new();
 static mut OLD_CONTEXT: OnceCell<AtomicPtr<HGLRC__>> = OnceCell::new();
 
 
@@ -309,21 +309,14 @@ fn swapbuffers_hook(hdc: winapi::shared::windef::HDC) -> winapi::ctypes::c_int {
 
     if is_ready.elapsed().unwrap().as_millis() > 5000 {
         unsafe {
-            let local_new_context = NEW_CONTEXT.get_mut().unwrap();
-            wglMakeCurrent(hdc, *local_new_context.get_mut());
+            *STATIC_HDC.get_mut().unwrap() = hdc;
+            if let Some(tx) = &*TX.lock().unwrap() {
+                tx.send(Message::RenderEvent).unwrap();
+            }
         }
-        render_event(hdc);
     }
 
 
-    
-    // send a message to the clickgui_thread to run the render event
-    unsafe {
-        //*STATIC_HDC.get_mut().unwrap() = hdc;
-        if let Some(_tx) = &*TX.lock().unwrap() {
-            // tx.send(Message::RenderEvent).unwrap();
-        }
-    }
 
     unsafe {
         let local_old_context = OLD_CONTEXT.get_mut().unwrap();
@@ -332,177 +325,7 @@ fn swapbuffers_hook(hdc: winapi::shared::windef::HDC) -> winapi::ctypes::c_int {
     call_original!(hdc)
 }
 
-fn render_event(hdc: HDC) {
-    // if enabled
-    if true {
-        unsafe {
-            //esp(*STATIC_HDC.lock().unwrap(),1.0)
-            esp(hdc, 1.0)
-        }
-    }
-}
-
-fn esp(hdc: HDC, _alpha: gl::types::GLfloat) {
-    /*
-    let rc_cli = null_mut();
-    unsafe {
-        GetClientRect(WindowFromDC(hdc), rc_cli);
-    }
-    let rc_cli = unsafe { *rc_cli };  // crash
-    let width = rc_cli.right - rc_cli.left;
-    let height = rc_cli.bottom - rc_cli.top;
-    */
-    draw_triangle(hdc, 400, 400);
-    /*
-    unsafe {
-        Viewport(0, 0, width, height);
-    }
-    */
-}
-
-fn draw_triangle(hdc: HDC, _w: i32, _h: i32) {
-    unsafe {
-        wglMakeCurrent(hdc, wglGetCurrentContext());
-    }
-
-    use gl::types::{GLfloat, GLenum, GLuint, GLchar, GLint, GLboolean, GLsizeiptr};
-    use std::str;
-    use std::ptr;
-    use std::mem;
-
-    static VERTEX_DATA: [GLfloat; 6] = [0.0, 0.5, 0.5, -0.5, -0.5, -0.5];
-    static VS_SRC: &str = "
-#version 150
-in vec2 position;
-void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
-}";
-
-    static FS_SRC: &str = "
-#version 150
-out vec4 out_color;
-void main() {
-    out_color = vec4(1.0, 1.0, 1.0, 1.0);
-}";
 
 
-    fn compile_shader(src: &str, ty: GLenum) -> GLuint {
-        let shader;
-        unsafe {
-            shader = gl::CreateShader(ty);
-            // Attempt to compile the shader
-            let c_str = CString::new(src.as_bytes()).unwrap();
-            gl::ShaderSource(shader, 1, &c_str.as_ptr(), ptr::null());
-            gl::CompileShader(shader);
-
-            // Get the compile status
-            let mut status = gl::FALSE as GLint;
-            gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
-
-            // Fail on error
-            if status != (gl::TRUE as GLint) {
-                let mut len = 0;
-                gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
-                let mut buf = Vec::with_capacity(len as usize);
-                buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
-                gl::GetShaderInfoLog(
-                    shader,
-                    len,
-                    ptr::null_mut(),
-                    buf.as_mut_ptr() as *mut GLchar,
-                );
-                panic!(
-                    "{}",
-                    str::from_utf8(&buf)
-                        .ok()
-                        .expect("ShaderInfoLog not valid utf8")
-                );
-            }
-        }
-        shader
-    }
-
-    fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
-        unsafe {
-            let program = gl::CreateProgram();
-            gl::AttachShader(program, vs);
-            gl::AttachShader(program, fs);
-            gl::LinkProgram(program);
-            // Get the link status
-            let mut status = gl::FALSE as GLint;
-            gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
-    
-            // Fail on error
-            if status != (gl::TRUE as GLint) {
-                let mut len: GLint = 0;
-                gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
-                let mut buf = Vec::with_capacity(len as usize);
-                buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
-                gl::GetProgramInfoLog(
-                    program,
-                    len,
-                    ptr::null_mut(),
-                    buf.as_mut_ptr() as *mut GLchar,
-                );
-                panic!(
-                    "{}",
-                    str::from_utf8(&buf)
-                        .ok()
-                        .expect("ProgramInfoLog not valid utf8")
-                );
-            }
-            program
-        }
-    }
-
-    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
-    let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
-    let program = link_program(vs, fs);
-
-    let mut vao = 0;
-    let mut vbo = 0;
 
 
-    unsafe {
-        // Create Vertex Array Object
-        gl::GenVertexArrays(1, &mut vao);
-        gl::BindVertexArray(vao);  // crash
-
-
-        
-        // Create a Vertex Buffer Object and copy the vertex data to it
-        gl::GenBuffers(1, &mut vbo);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-            mem::transmute(&VERTEX_DATA[0]),
-            gl::STATIC_DRAW,
-        );
-
-
-        // Use shader program
-        gl::UseProgram(program);
-        gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
-        
-        // Specify the layout of the vertex data
-        let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
-        gl::EnableVertexAttribArray(pos_attr as GLuint);
-        gl::VertexAttribPointer(
-            pos_attr as GLuint,
-            2,
-            gl::FLOAT,
-            gl::FALSE as GLboolean,
-            0,
-            ptr::null(),
-        );
-    }
-
-    unsafe {
-        // Clear the screen to black
-        gl::ClearColor(0.7, 0.1, 0.1, 1.0);
-        gl::Clear(gl::COLOR_BUFFER_BIT);
-        // Draw a triangle from the 3 vertices
-        gl::DrawArrays(gl::TRIANGLES, 0, 3);
-    }
-}
